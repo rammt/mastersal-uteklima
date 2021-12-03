6d01081630d9be2897f62a72c2578570536229c5
Magnuti <magnustidemann@gmail.com>
2021-12-03 14:57:41 +0100

    GPIO for transistor

--- raspberry_pi/gpio_controller.py
+++ raspberry_pi/gpio_controller.py
@@ -1,6 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.output(1, GPIO.HIGH)
-time.sleep(2)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.HIGH)
+time.sleep(1)
 GPIO.output(1, GPIO.LOW)
+GPIO.cleanup()
