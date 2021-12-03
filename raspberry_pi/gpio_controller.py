9bf711a6a6e2df5ed4d827d7fe60c92acd724c35
Magnuti <magnustidemann@gmail.com>
2021-12-03 15:17:43 +0100

    Pin fix

--- raspberry_pi/gpio_controller.py
+++ raspberry_pi/gpio_controller.py
@@ -4,7 +4,7 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(17, GPIO.OUT)
-GPIO.output(17, GPIO.HIGH)
+GPIO.output(17, GPIO.LOW)
 time.sleep(1)
-GPIO.output(1, GPIO.LOW)
+GPIO.output(1, GPIO.HIGH)
 GPIO.cleanup()
