e57729c2e5858b5eb15bc31317d6bebaf6985578
Magnuti <magnustidemann@gmail.com>
2021-12-07 12:46:37 +0100

    sh fix

--- raspberry_pi/start_script.sh
+++ raspberry_pi/start_script.sh
@@ -3,7 +3,7 @@
 # docker stop $(docker ps -q --filter ancestor=rasp-app)
 # docker run -p 4000:4000 -d rasp-app
 lt -s pretty-mastersal-40 --port 4000 &
-npm install; && npm start; &
+npm install && npm start &
 wait
 #curl --location --request POST 'http://localhost:4001/new_url' \
 #--header 'Content-Type: application/json' \
