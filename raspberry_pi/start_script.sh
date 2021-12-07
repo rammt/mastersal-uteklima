d95104ca882673532073b918b097d8903d108e18
Magnuti <magnustidemann@gmail.com>
2021-12-07 13:01:40 +0100

    URL fix

--- raspberry_pi/start_script.sh
+++ raspberry_pi/start_script.sh
@@ -2,9 +2,9 @@
 
 # docker stop $(docker ps -q --filter ancestor=rasp-app)
 # docker run -p 4000:4000 -d rasp-app
-lt -s pretty-mastersal-40 --port 4000 &
+lt -s pretty-mastersal-50 --port 4000 &
 npm install && npm start &
 wait
 #curl --location --request POST 'http://localhost:4001/new_url' \
 #--header 'Content-Type: application/json' \
-#--data-raw '{"url": "pretty-mastersal-40"}'
+#--data-raw '{"url": "pretty-mastersal-50"}'
